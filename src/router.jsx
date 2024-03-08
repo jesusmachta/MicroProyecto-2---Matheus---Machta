e6d5bc06fa2dce6b0172761d8e7486314f5f8c68
Jesus Machta <97565151+jesusmachta@users.noreply.github.com>
2024-03-08 12:03:37 -0400

    :construction: primeros pasos

---
+++ src/router.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from 'react-router-dom';
+import ClubDetails from './pages/ClubDetails';
+import LandingPage from './pages/LandingPage';
+import LogInPage from './pages/LogInPage';
+import RegisterPage from './pages/RegisterPage';
+import SearchPage from './pages/SearchPage';
+
+export const router = createBrowserRouter([
+    {
+      path: '/',
+      elemment: <LandingPage />,
+    },
+
+    {
+      path: '/login',
+      element: <LogInPage />,
+    },
+
+    {
+      path: '/register',
+      element: <RegisterPage />,
+    },
+
+    {
+      path: '/search',
+      element: <SearchPage />,
+    },
+
+    {
+      path: '/club',
+      element: <ClubDetails />,
+    }
+
+  ]);
+  
