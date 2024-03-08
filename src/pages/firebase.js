e2594fc2a925bdd294e29dcb41e862fa51975d3f
Jesus Machta <97565151+jesusmachta@users.noreply.github.com>
2024-03-08 10:57:28 -0400

    :fire: primer commit, firebase listo y el npm tmbn

---
+++ src/pages/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD8_pfZBytS-RrXGdmtMLkS8gkXR63njGg",
+  authDomain: "microproyecto2-matheus-m-cdd7d.firebaseapp.com",
+  projectId: "microproyecto2-matheus-m-cdd7d",
+  storageBucket: "microproyecto2-matheus-m-cdd7d.appspot.com",
+  messagingSenderId: "36622843713",
+  appId: "1:36622843713:web:03472320d51a0ce1d56bc0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
