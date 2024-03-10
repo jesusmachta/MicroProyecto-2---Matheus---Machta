ae2984d75246bb52fe998b50abad8c3ace5e01a5
Jesus Machta <97565151+jesusmachta@users.noreply.github.com>
2024-03-10 10:29:35 -0400

    :beers: agregue el boton para que el usuario se una al club en la vista detallada y puse el responsive en el navbar

--- src/components/ClubDetail.jsx
+++ src/components/ClubDetail.jsx
@@ -1,34 +1,44 @@
-// ClubDetail.jsx
-import { useState } from 'react';
-import PropTypes from 'prop-types';
-import styles from './ClubDetail.module.css';
-import VideoJuegoDetail from './VideoJuegoDetail';
-import Navbar from '../pages/Navbar';
-
+import { useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./ClubDetail.module.css";
+import VideoJuegoDetail from "./VideoJuegoDetail";
+import Navbar from "../pages/Navbar";
 export default function ClubDetail({ club }) {
   const [selectedVideojuego, setSelectedVideojuego] = useState(null);
 
   if (selectedVideojuego) {
-    return <VideoJuegoDetail videojuego={selectedVideojuego}/>;
+    return <VideoJuegoDetail videojuego={selectedVideojuego} />;
   }
+
+  const handleJoin = () => {
+    console.log("Unirse al club");
+  };
 
   return (
     <div className={styles.root}>
-        <Navbar />
-      <h1 className={styles.nombreClub}>{club.nombre}</h1> 
-      <p className={styles.descripcionClub}>{club.descripcion}</p> 
-      
+      <Navbar />
+      <h1 className={styles.nombreClub}>{club.nombre}</h1>
+      <p className={styles.descripcionClub}>{club.descripcion}</p>
+
       <div>
         <p>Juegos que utilizamos en nuestro club</p>
       </div>
-      
-      <div className={styles['cards-container']}>
+
+      <div className={styles["cards-container"]}>
         {club.videojuegos.map((videojuego) => (
-          <div key={videojuego} className={styles.card} onClick={() => setSelectedVideojuego(videojuego)}>
+          <div
+            key={videojuego}
+            className={styles.card}
+            onClick={() => setSelectedVideojuego(videojuego)}
+          >
             <img src={`/${videojuego}.png`} alt={videojuego} />
           </div>
         ))}
       </div>
+
+      <button className={styles.joinButton} onClick={handleJoin}>
+        Unirse al club
+      </button>
     </div>
   );
 }
